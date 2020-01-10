251f86cf5bb6af7586dcc8dac08e3e6bfc7c3cab
Georg Oehl <oehl@pobox.com>
2020-01-10 00:31:48 -0500

    En route to implementing Up/Down-voting

--- src/pages/SingleQuestionPage.jsx
+++ src/pages/SingleQuestionPage.jsx
@@ -22,11 +22,87 @@ const SingleQuestionPage = props => {
     getSingleQuestion()
   }, [])
 
-  const setSingleQuestion = () => {}
+  const setNewVoteValue = async (answerOrQuestion, indexOrId, deltaValue) => {
+    const obj = {}
+    switch (answerOrQuestion) {
+      case 'Question':
+        obj.id = indexOrId
+        obj.questionTitle = questionData.questionTitle
+        obj.questionText = questionData.questionText
+        obj.voteValue = questionData.voteValue + deltaValue
+        break
+      case 'Answer':
+        obj.id = questionData.answers[indexOrId].id
+        obj.questionId = questionData.id
+        obj.answerText = questionData.answers[indexOrId].answerText
+        obj.voteValue = questionData.answers[indexOrId].voteValue + deltaValue
+        break
+      default:
+        break
+    }
+    console.log(`Would have now updated ${answerOrQuestion} as follows:`)
+    console.dir(obj)
+  }
 
+  // prettier-ignore
   const upDownVote = e => {
     e.persist()
+    
+    let qOrA
+    let indexOrId
+    let deltaValue
+    
     console.log('Pressed ' + e.currentTarget.name + `(${e.currentTarget.id})`) // e.target.parentNode.tagName) // e.target.className.baseVal)
+
+    qOrA = e.currentTarget.name.substring(0,1)
+    switch (qOrA) {
+      case 'q':
+        qOrA = 'Question'
+        indexOrId = e.currentTarget.id
+        break
+      case 'a':
+        qOrA = 'Answer'
+        // Find the array index of the element in the answers array to be updated
+        indexOrId = questionData.answers.map(a => a.id.toString()).indexOf(e.currentTarget.id)
+        break
+      default:
+        console.log(`Question/Answer ${qOrA} not identified`)
+        return
+        break
+    }
+    switch (e.currentTarget.name.substring(1,3)) {
+      case 'Up':
+        deltaValue = 1
+        break
+      case 'Dn':
+        deltaValue = -1
+        break
+      default:
+        console.log("Up/Down not identified")
+        return
+        break
+    }
+    setNewVoteValue(qOrA,indexOrId,deltaValue)
+
+    // switch (e.currentTarget.name) {
+    //   case 'qUpVote':
+    //     console.log('current vote value: ' + questionData.voteValue + ' to be incremented')
+    //     break
+    //   case 'qDnVote':
+    //     console.log('current vote value: ' + questionData.voteValue + ' to be decremented')
+    //     break
+    //   case 'aUpVote':
+    //     console.log('current vote value: ' + questionData.answers[questionData.answers.map(a => a.id.toString()).indexOf(e.currentTarget.id)].voteValue + ' to be incremented')
+    //     // console.log('answer array element to be updated: ' + questionData.answers.map(a => a.id.toString()).indexOf(e.currentTarget.id))
+    //     break
+    //   case 'aDnVote':
+    //     console.log('current vote value: ' + questionData.answers[questionData.answers.map(a => a.id.toString()).indexOf(e.currentTarget.id)].voteValue + ' to be decremented')
+    //     // console.log('answer array element to be updated: ' + questionData.answers.map(a => a.id.toString()).indexOf(e.currentTarget.id))
+    //     break
+
+    //   default:
+    //     break
+    // }
     // console.dir(e.target)
   }
 
