04eac9ea5e2106f76376ba4c80a88684e956b860
Nathanael Swander <swander926@yahoo.com>
2020-01-11 22:58:17 -0500

    added ask page css

--- src/pages/AskPage.jsx
+++ src/pages/AskPage.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 import axios from 'axios'
 
 const Page2 = () => {
-  return <div>Ask Page</div>
+  return (
+    <>
+      <div className="askContainer">
+        <p className="askInstructions">Ask a public question</p>
+        <div className="askBox">
+          <h2 className="askTitle">Title</h2>
+          <p className="askTitleInstructions">Be specific and imagine you're asking a question to another person</p>
+          <input className="askTitleInput" type="text"></input>
+          <h2 className="askBody">Body</h2>
+          <p className="askBodyInstructions">Include all the information someone would need to answer your question</p>
+          <textarea className="askBodyInput" type="text" cols="156" rows="15"></textarea>
+        </div>
+        <button className="askButton" type="submit">
+          Post your question
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default Page2
